9df65064de9d6d93fec6678148749ae415d62d8f
Paulo <pescanellasotero@gmail.com>
2025-11-25 09:16:48 +0100

    Integramos todo el código de la carpeta notes en una clase NotesController

---
+++ demo/Http/controllers/NotesController.php
@@ -0,0 +1,151 @@
+<!-- create.php !-->
+<?php
+
+view("notes/create.view.php", [
+    'heading' => "Create Note",
+    'errors' => []
+]);
+
+
+//Todos los use
+use Core\App;
+use Core\Database;
+use Core\Validator;
+
+//destroy.php
+
+$db = App::resolve(Database::class);
+
+$currentUserId = 2;
+
+$note = $db->query('SELECT * FROM notes WHERE id = :id', [
+    'id' => $_GET['id'],
+])->findOrFail();
+
+authorize($note['user_id'] === $currentUserId);
+
+$db->query("DELETE FROM notes WHERE id = :id", ["id" => $_GET["id"]]);
+
+header('location: /notes');
+exit();
+
+//edit.php
+
+$db = App::resolve(Database::class);
+
+$currentUserId = 2;
+
+$note = $db->query('SELECT * FROM notes WHERE id = :id', [
+    'id' => $_GET['id'],
+])->findOrFail();
+
+authorize($note['user_id'] === $currentUserId);
+
+view("notes/edit.view.php", [
+    'heading' => "Edit Note",
+    'errors' => [],
+    'note' => $note,
+]);
+
+//index.php
+
+$db = App::resolve(Database::class);
+
+$userName = $_SESSION['user']['name'];
+
+$user = $db->query("SELECT id FROM users WHERE name = :username", [
+    'username' => $userName
+])->find();
+
+$notes = $db->query('SELECT * FROM notes WHERE user_id = :user_id', [
+    'user_id' => $user['id']
+])->get();
+
+view("notes/index.view.php", [
+    'heading' => "Notes",
+    'notes' => $notes,
+]);
+
+//show.php
+
+$db = App::resolve(Database::class);
+
+$currentUserId = 2;
+
+$note = $db->query('SELECT * FROM notes WHERE id = :id', [
+    'id' => $_GET['id'],
+])->findOrFail();
+
+authorize($note['user_id'] === $currentUserId);
+
+view("notes/show.view.php", [
+    'heading' => "Note nº" . $_GET['id'],
+    'note' => $note,
+]);
+
+//store.php
+
+$db = App::resolve(Database::class);
+$errors = [];
+
+if (!Validator::string($_POST["body"], 1, 1000)) {
+    $errors['body'] = "A body of no more than 1,000 characters is required";
+}
+
+if (!empty($errors)) {
+    return view("notes/create.view.php", [
+        'heading' => "Create Note",
+        'errors' => $errors
+    ]);
+}
+
+if (!isset($_SESSION['user'])) {
+    die('No hay sesión de usuario válida');
+}
+
+$userName = $_SESSION['user']['name'];
+$user = $db->query("SELECT id FROM users WHERE name = :username", [
+    'username' => $userName
+])->find();
+
+$db->query('INSERT INTO notes (body, user_id) VALUES (:body, :user_id)', [
+    'body'    => $_POST['body'],
+    'user_id' => $user['id']
+]);
+
+header('location: /notes');
+exit();
+
+//update.php
+
+$db = App::resolve(Database::class);
+
+$currentUserId = 2;
+
+$note = $db->query('SELECT * FROM notes WHERE id = :id', [
+    ':id' => $_POST['id']
+])->findOrFail();
+
+authorize($note['user_id'] === $currentUserId);
+
+$errors = [];
+
+if (!Validator::string($_POST["body"], 1, 1000)) {
+    $errors['body'] = "A body of no more than 1,000 characters is required";
+}
+
+if (count($errors)) {
+    return view("notes/edit.view.php", [
+        'heading' => 'Edit note',
+        'errors' => $errors,
+        'note' => $note
+    ]);
+}
+
+$db->query('UPDATE notes SET body = :body WHERE id = :id', [
+    'id' => $_POST['id'],
+    'body' => $_POST['body']
+]);
+
+header("location: /notes");
+die();
