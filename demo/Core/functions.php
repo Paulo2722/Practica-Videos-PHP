934b7ae4541162298d025acb9877e1c8a3fc3147
Paulo <pescanellasotero@gmail.com>
2025-11-28 20:25:48 +0100

    Corrección de la función abort

--- demo/Core/functions.php
+++ demo/Core/functions.php
@@ -20,7 +20,7 @@ function abort($code = 404)
 {
     http_response_code($code);
 
-    require base_path('views') . 'functions.php/' . $code . '.php';
+    require base_path('views/' . $code . '.php');
 
     die();
 }
