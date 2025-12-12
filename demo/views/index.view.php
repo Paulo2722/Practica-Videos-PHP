c32bf932ebfee9ab8b84af90ab51f869b9f140d8
Paulo <pescanellasotero@gmail.com>
2025-12-12 09:53:55 +0100

    Elimino cambios anteriores

--- demo/views/index.view.php
+++ demo/views/index.view.php
@@ -5,14 +5,6 @@
     <main>
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
             <p class="text-white">Hello <?= $_SESSION['user']['name'] ?? "Guest" ?>. Welcome to the home page.</p>
-
-            <!--Campos para añadir el cumpleaños y el telefono!-->
-            <p>*Estos campos estan por el examen*</p>
-            <form method="post">
-                <input type="date" style="color: blue">
-                <input type="text" style="color: blue">
-                <button type="submit">Guardar</button>
-            </form>
         </div>
     </main>
 
