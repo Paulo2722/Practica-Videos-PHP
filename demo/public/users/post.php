334ebe7c8ee7d99578bcd9cb4e8fdd38c843a00f
Paulo <pescanellasotero@gmail.com>
2025-12-12 10:28:47 +0100

    Añadir datos de usuario

--- demo/public/users/post.php
+++ demo/public/users/post.php
@@ -1,3 +1,14 @@
 <?php
+
 include("../../Core/Database.php");
-$sql =
+$sql = "select * from users insert into users(number) values(123456789)";
+
+$result = $this->connection->query($sql);
+
+$data = [];
+
+while ($row = $result->fetch_assoc()) {
+    $data[] = $row;
+}
+
+echo json_encode($data);
