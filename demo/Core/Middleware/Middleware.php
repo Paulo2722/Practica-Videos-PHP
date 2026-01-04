7e50aca935b8665587152466aca7b567d2377158
Paulo <pescanellasotero@gmail.com>
2026-01-04 22:33:40 +0100

    Añado la clase AuthApi al Middleware.php para proteger las rutas

--- demo/Core/Middleware/Middleware.php
+++ demo/Core/Middleware/Middleware.php
@@ -7,6 +7,7 @@ class Middleware
     const MAP = [
         'guest' => Guest::class,
         'auth' => Auth::class,
+        'auth_api' => AuthApi::class,
     ];
 
     public static function resolve($key)
