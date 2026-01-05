de5269dce716fb5510562e2954761d5b2ddbc93d
Paulo <pescanellasotero@gmail.com>
2026-01-05 21:40:05 +0100

    Código para borrar el token actual

--- demo/API/logout.php
+++ demo/API/logout.php
@@ -1,1 +1,10 @@
 <?php
+use Core\Authenticator;
+use Core\Middleware\Middleware;
+use function Core\json;
+
+Middleware::resolve('auth.api');
+
+(new Authenticator)->eliminarToken($_SERVER['api_token']);
+
+json(['message' => 'Token eliminado']);
