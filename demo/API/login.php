a6c6e65f5a5e0a0a0f541d43daa40b168e23a065
Paulo <pescanellasotero@gmail.com>
2026-01-05 21:35:21 +0100

    Código para autenticar un usuario

--- demo/API/login.php
+++ demo/API/login.php
@@ -1,1 +1,14 @@
 <?php
+
+use Core\Authenticator;
+use function Core\json;
+
+$auth = new Authenticator();
+
+$result = $auth->attempt($_POST['email'], $_POST['password']);
+
+if (!$result) {
+    json(['error' => 'Credenciales incorrectas'], 401);
+}
+
+json($result);
