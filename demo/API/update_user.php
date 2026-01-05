6220138d6c04fc7337e83c39168217a633023714
Paulo <pescanellasotero@gmail.com>
2026-01-05 21:39:38 +0100

    Código para actualizar los datos de un usuario

--- demo/API/update_user.php
+++ demo/API/update_user.php
@@ -1,1 +1,20 @@
 <?php
+use Core\App;
+use Core\Database;
+use Core\Middleware\Middleware;
+use function Core\json;
+
+Middleware::resolve('auth.api');
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+App::resolve(Database::class)->query(
+    "UPDATE users SET birth_date = :birth, phone = :phone WHERE id = :id",
+    [
+        'birth' => $data['birth_date'],
+        'phone' => $data['phone'],
+        'id' => $_SERVER['api_user']['id']
+    ]
+);
+
+json(['message' => 'Datos actualizados']);
